70ccb46b327a80b49fe6b7d2190b40a8c690d4ed
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-04 06:09:22 +0100

    logistic system

--- src/main/java/africa/semicolon/cheetahApp/data/repositories/PackageRepositoryImpl.java
+++ src/main/java/africa/semicolon/cheetahApp/data/repositories/PackageRepositoryImpl.java
@@ -1,2 +1,47 @@
-package africa.semicolon.cheetahApp.data.repositories;public class PackageRepositoryImpl {
+package africa.semicolon.cheetahApp.data.repositories;
+
+import africa.semicolon.cheetahApp.data.models.Package;
+
+import java.util.*;
+
+public class PackageRepositoryImpl implements PackageRepository{
+
+    private final Map<Integer, Package> database = new HashMap<>();
+
+    @Override
+    public Package save(Package aPackage) {
+        Integer id = null;
+        if (aPackage.getId() == null){
+         id = database.size() + 1;
+        aPackage.setId(id);
+        }
+        id = aPackage.getId();
+        database.put(id,aPackage);
+        return database.get(id);
+    }
+
+    @Override
+    public List<Package> findAll() {
+        List<Package> packages = new ArrayList<>();
+        Set<Integer>keys = database.keySet();
+        for(Integer key : keys){
+            packages.add(database.get(key));
+        }
+        return packages;
+    }
+
+    @Override
+    public void delete(Package aPackage) {
+        database.remove(aPackage.getId());
+    }
+
+    @Override
+    public void delete(Integer trackingNumber) {
+        database.remove(trackingNumber);
+    }
+
+    @Override
+    public Package findById(Integer id) {
+        return database.get(id);
+    }
 }
