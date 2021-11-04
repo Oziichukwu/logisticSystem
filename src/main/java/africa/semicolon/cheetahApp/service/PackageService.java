1489114f306fe7bb7ce508cae33bfd6d0d5c6460
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-04 07:05:16 +0100

    logistic app

--- src/main/java/africa/semicolon/cheetahApp/service/PackageService.java
+++ src/main/java/africa/semicolon/cheetahApp/service/PackageService.java
@@ -1,2 +1,13 @@
-package africa.semicolon.cheetahApp.service;public interface PackageService {
+package africa.semicolon.cheetahApp.service;
+
+import africa.semicolon.cheetahApp.data.dtos.request.AddPackageRequest;
+import africa.semicolon.cheetahApp.data.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetahApp.data.models.Package;
+
+public interface PackageService {
+
+
+    AddPackageResponse addPackage(AddPackageRequest addPackageRequest);
+
+    Package findMyPackageWith(Integer id);
 }
--- src/main/java/africa/semicolon/cheetahApp/service/PackageServiceImpl.java
+++ src/main/java/africa/semicolon/cheetahApp/service/PackageServiceImpl.java
@@ -1,2 +1,30 @@
-package africa.semicolon.cheetahApp.service;public class PackageServiceImpl {
+package africa.semicolon.cheetahApp.service;
+
+import africa.semicolon.cheetahApp.data.dtos.request.AddPackageRequest;
+import africa.semicolon.cheetahApp.data.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetahApp.data.models.Package;
+import africa.semicolon.cheetahApp.data.repositories.PackageRepository;
+import africa.semicolon.cheetahApp.data.repositories.PackageRepositoryImpl;
+import africa.semicolon.cheetahApp.utils.ModelMapper;
+
+public class PackageServiceImpl implements PackageService {
+    private final PackageRepository packageRepository = new PackageRepositoryImpl();
+
+    @Override
+    public AddPackageResponse addPackage(AddPackageRequest addPackageRequest) {
+
+        Package aPackage = ModelMapper.map(addPackageRequest);
+
+        Package savedPackage = packageRepository.save(aPackage);
+
+        AddPackageResponse response = ModelMapper.map(savedPackage);
+
+        return response;
+
+    }
+
+    @Override
+    public Package findMyPackageWith(Integer id) {
+        return packageRepository.findById(id);
+    }
 }
