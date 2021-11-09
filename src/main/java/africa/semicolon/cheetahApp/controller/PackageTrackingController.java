f4ca1b329bc03c394e161d490851295df70189f5
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-09 23:21:57 +0100

    logistic updated

--- src/main/java/africa/semicolon/cheetahApp/controller/PackageTrackingController.java
+++ src/main/java/africa/semicolon/cheetahApp/controller/PackageTrackingController.java
@@ -1,2 +1,25 @@
-package africa.semicolon.cheetahApp.controller;public class PackageTrackingController {
+package africa.semicolon.cheetahApp.controller;
+
+
+import africa.semicolon.cheetahApp.data.dtos.request.AddTrackingInfoRequest;
+import africa.semicolon.cheetahApp.data.dtos.responses.AddTrackingInfoResponse;
+import africa.semicolon.cheetahApp.data.models.TrackingInformation;
+import africa.semicolon.cheetahApp.service.TrackingService;
+import africa.semicolon.cheetahApp.service.TrackingServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PackageTrackingController {
+    private final TrackingService trackingService = new TrackingServiceImpl();
+
+    @PostMapping("/api/trackingInfo")
+    public AddTrackingInfoResponse addTrackingInfo(@RequestBody AddTrackingInfoRequest addTrackingInfoRequest){
+        return trackingService.updateTrackingInfo(addTrackingInfoRequest);
+    }
+
+    @GetMapping("/api/trackingInfo{id}")
+    public TrackingInformation getTrackingInfo(@PathVariable ("id") Integer trackingId){
+
+        return trackingService.trackPackage(trackingId);
+    }
 }
