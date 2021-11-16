3a64aec820f6e21c2cb11ba2bcb67b5010d9f50e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-16 17:21:26 +0100

    logistic app

--- src/main/java/africa/semicolon/cheetahApp/controller/PackageController.java
+++ src/main/java/africa/semicolon/cheetahApp/controller/PackageController.java
@@ -11,6 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 public class PackageController {
