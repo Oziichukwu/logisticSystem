501eda57131bacb050b13dac4926a17f054c0af4
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-12-08 12:15:25 +0100

    added new

--- src/main/java/africa/semicolon/cheetahApp/data/exceptions/DuplicatePackageIdException.java
+++ src/main/java/africa/semicolon/cheetahApp/data/exceptions/DuplicatePackageIdException.java
@@ -3,6 +3,7 @@ package africa.semicolon.cheetahApp.data.exceptions;
 public class DuplicatePackageIdException extends LogisticAppException{
 
     public DuplicatePackageIdException(String message) {
+
         super(message);
     }
 }
